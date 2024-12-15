a5ac6bdfbe726f487f11ba706c3efa911d79354d
Francesco Di Niccolo <francescodiniccolo@gmial.com>
2024-12-15 07:51:40 +0000

    ...

--- 2024/05.py
+++ 2024/05.py
@@ -35,6 +35,30 @@ with open("05.txt") as _:
     puzzle_input = _.read().strip()
 
 
+def get_mid_value(lst):
+    return lst[len(lst)//2]
+
+
+def check_update(update):
+    for index, page in enumerate(update):
+        if index+1 < len(update) and page in not_to_be_followed_by.keys():
+            remaining_pages = set(update[index+1:])
+            intersection = not_to_be_followed_by[page] & remaining_pages
+            if intersection:
+                return False, (index, list(intersection)[0])
+    return True, (None, None)
+
+
+def sort_update(update):
+    while True:
+        result, (index_a, intersection) = check_update(update)
+        if result:
+            return update
+        else:
+            index_b = update.index(intersection)
+            update[index_a], update[index_b] = update[index_b], update[index_a]
+
+
 rules, updates = puzzle_input.split('\n\n')
 
 rules = [
@@ -50,23 +74,25 @@ for value, key in rules:
     else:
         not_to_be_followed_by[key].add(value)
 
-
 updates = [
     list(map(int, item.split(',')))
     for item in updates.split('\n')
 ]
 
+middle_values = [
+    get_mid_value(update) for update in updates
+    if check_update(update)[0]
+]
 
-def check_update(update):
-    for index, page in enumerate(update):
-        if index+1 < len(update) and page in not_to_be_followed_by.keys():
-            remaining_pages = set(update[index+1:])
-            if not_to_be_followed_by[page] & remaining_pages:
-                return
-    return update[len(update)//2]
+print(sum(middle_values))
 
+sorted_updates = [
+    sort_update(update) for update in updates
+    if not check_update(update)[0]
+]
 
-middle_values = [middle_value for update in updates if (
-    middle_value := check_update(update)) is not None]
+middle_values = [
+    get_mid_value(update) for update in sorted_updates
+]
 
 print(sum(middle_values))
