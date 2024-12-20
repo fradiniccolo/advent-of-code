cee39100fa3fd27d72c781c05ec70aaec7151b46
Francesco Di Niccolo <francescodiniccolo@gmail.com>
2024-12-20 07:17:15 +0000

    ...

--- 2024/07.py
+++ 2024/07.py
@@ -1,61 +1,55 @@
-from itertools import product
-
-
 with open("07.txt") as _:
     puzzle_input = _.read().strip()
 
 equations = puzzle_input.split('\n')
 equations = [equation.split(': ') for equation in equations]
 equations = [(total, numbers.split()) for total, numbers in equations]
-equations = [(int(total), tuple(map(int, numbers)))
+equations = [(int(total), list(map(int, numbers)))
              for total, numbers in equations]
 
 
-def get_operators_combinations(operators, operators_count):
-    combinations = list(product(operators, repeat=operators_count))
-    order = {key: value for value, key in enumerate(['+', '*', '||'])}
-    order = sorted(combinations, key=lambda o: order.get(o, float('inf')))
-    order.reverse()
-    # print(order)
-    return order
+def calculate(first, operator, second):
+    if operator == '||':
+        return eval(f"{first}{second}")
+    return eval(f"{first}{operator}{second}")
 
 
-def is_equation_true(equation, operators):
-    total, numbers = equation
-    operators_count = len(numbers)-1
-    operators_combinations = get_operators_combinations(
-        operators, operators_count)
-
-    for combination in operators_combinations:
-        result = numbers[0]
-        for operator, number in zip(combination, numbers[1:]):
-            if operator != '||':
-                result = eval(f"{result}{operator}{number}")
-            else:
-                result = eval(f"{result}{number}")
-            if result > total:
-                # return False
-                break
-        if result == total:
-            return True
+def is_equation_true(test_value, numbers, operators):
+    if len(numbers) == 2:
+        first, second = numbers
+        for operator in operators:
+            result = calculate(first, operator, second)
+            if result > test_value:  # save computation
+                continue
+            if result == test_value:
+                return True
+        return False
+    else:
+        first, second = numbers[:2]
+        remaining = numbers[2:]
+        for operator in operators:
+            result = calculate(first, operator, second)
+            if result > test_value:  # save computation
+                continue
+            less_numbers = [result] + remaining
+            if is_equation_true(test_value, less_numbers, operators):
+                return True
     return False
 
 
 operators = ['+', '*']
 total_calibration = 0
-for equation in equations:
-    if is_equation_true(equation, operators):
-        total_calibration += equation[0]
+for test_value, numbers in equations:
+    if is_equation_true(test_value, numbers, operators):
+        total_calibration += test_value
 
 print(total_calibration)
 
 
 operators = ['+', '*', '||']
 total_calibration = 0
-for equation in equations:
-    if is_equation_true(equation, operators):
-        total_calibration += equation[0]
+for test_value, numbers in equations:
+    if is_equation_true(test_value, numbers, operators):
+        total_calibration += test_value
 
 print(total_calibration)
-
-# 12839601725877
