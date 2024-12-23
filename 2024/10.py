fa72752e73b1f6f17cd8b8ead2a74cac19fdbc15
Francesco Di Niccolo <francescodiniccolo@gmail.com>
2024-12-23 16:39:10 +0000

    ...

--- 2024/10.py
+++ 2024/10.py
@@ -14,14 +14,16 @@ class Trailer:
     def __init__(self, head):
         self.head = head
         self.tails = set()
+        self.rating = 0
         self.find_tails(self.head)
-        self.trailer_score = len(self.tails)
+        self.score = len(self.tails)
 
     def find_tails(self, current_node):
         if current_node.value == 9:
             self.tails.add(current_node)
+            self.rating += 1
         else:
-            for connected_node in current_node.connections:
+            for connected_node in current_node.reachable_nodes:
                 self.find_tails(connected_node)
 
 
@@ -33,7 +35,8 @@ class Topomap:
         self.nodes = {}
         self.setup()
         self.map_score = 0
-        self.get_map_score()
+        self.map_rating = 0
+        self.get_map_score_and_rating()
 
     def setup(self):
         for row_idx, row in enumerate(self.matrix):
@@ -43,28 +46,30 @@ class Topomap:
             if x > 0:
                 west_node = self.nodes[(x-1, y)]
                 if west_node.value - node.value == 1:
-                    node.connections.append(west_node)
+                    node.reachable_nodes.append(west_node)
             if x < self.size-1:
                 east_node = self.nodes[(x+1, y)]
                 if east_node.value - node.value == 1:
-                    node.connections.append(east_node)
+                    node.reachable_nodes.append(east_node)
             if y > 0:
                 north_node = self.nodes[(x, y-1)]
                 if north_node.value - node.value == 1:
-                    node.connections.append(north_node)
+                    node.reachable_nodes.append(north_node)
             if y < self.size-1:
                 south_node = self.nodes[(x, y+1)]
                 if south_node.value - node.value == 1:
-                    node.connections.append(south_node)
+                    node.reachable_nodes.append(south_node)
 
     def get_trailers(self):
         trailheads = [node for node in self.nodes.values() if node.value == 0]
         return [Trailer(trailhead) for trailhead in trailheads]
 
-    def get_map_score(self):
+    def get_map_score_and_rating(self):
         for trailer in self.get_trailers():
-            self.map_score += trailer.trailer_score
+            self.map_score += trailer.score
+            self.map_rating += trailer.rating
 
 
 topomap = Topomap(puzzle_input)
 print(topomap.map_score)
+print(topomap.map_rating)
