9c94bde66b3b4fc563beb88026c0f4edf85d7aff
Francesco Di Niccolo <francescodiniccolo@gmail.com>
2024-12-24 10:04:00 +0000

    ...

--- 2024/11.py
+++ 2024/11.py
@@ -1,32 +1,39 @@
-from collections import deque
-
 with open("11.txt") as _:
     puzzle_input = _.read().strip()
 
 
-stones = [int(stone) for stone in puzzle_input.split()]
+class Stone:
+
+    stone_count = 0
+
+    def __init__(self, number, *, blink_count=0, max_blinks):
+        Stone.stone_count += 1
+        self.number = number
+        self.blink_count = blink_count
+        self.max_blinks = max_blinks
+        self.blink()
+
+    def get_next(self):
+        if self.number == 0:
+            return [1]
+        number_digits = len(str(self.number))
+        if number_digits % 2 == 0:
+            return [
+                int(str(self.number)[:number_digits//2]),
+                int(str(self.number)[number_digits//2:]),
+            ]
+        return [self.number*2024]
+
+    def blink(self):
+        if self.blink_count < self.max_blinks:
+            Stone.stone_count -= 1
+            for number in self.get_next():
+                Stone(number, blink_count=self.blink_count +
+                      1, max_blinks=self.max_blinks)
 
 
-def blink(stone):
-    if stone == 0:
-        return 1
-    len_digits = len(str(stone))
-    if len_digits % 2 == 0:
-        halves = [
-            int(str(stone)[:len_digits//2]),
-            int(str(stone)[len_digits//2:]),
-        ]
-        return halves
-    return stone*2024
+stones = [Stone(int(item), max_blinks=25) for item in puzzle_input.split()]
+print(Stone.stone_count)
 
-
-for _ in range(25):
-    changing_stones = deque()
-    for stone in stones:
-        new_stone = blink(stone)
-        if isinstance(new_stone, list):
-            changing_stones += new_stone
-        else:
-            changing_stones.append(new_stone)
-    stones = changing_stones
-print(len(stones))
+stones = [Stone(int(item), max_blinks=75) for item in puzzle_input.split()]
+print(Stone.stone_count)
