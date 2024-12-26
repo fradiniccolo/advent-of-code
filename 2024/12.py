64f69296a0c944679fbe63fdb2fbabf380835d09
Francesco Di Niccolo <francescodiniccolo@gmail.com>
2024-12-26 11:29:14 +0000

    almost there

--- 2024/12.py
+++ 2024/12.py
@@ -1,7 +1,3 @@
-with open("12.txt") as _:
-    puzzle_input = _.read().strip()
-
-
 class Garden:
 
     def __init__(self, map_text):
@@ -14,7 +10,7 @@ class Garden:
         self.get_plots_from_input(input)
         self.get_regions()
         self.get_region_areas()
-        self.get_region_perimeters()
+        self.get_region_perimeters_and_sides()
 
     def get_plots_from_input(self, text):
 
@@ -28,18 +24,27 @@ class Garden:
 
         # assign neighbours to plots
         for (x, y), node in plots.items():
-            if x > 0:
-                west_node = plots[(x-1, y)]
-                node.neighbours.append(west_node)
+
+            east_node = Plot((x+1, y), None)
             if x < self.size-1:
                 east_node = plots[(x+1, y)]
-                node.neighbours.append(east_node)
+            node.neighbours.append(east_node)
+
+            north_node = Plot((x, y-1), None)
             if y > 0:
                 north_node = plots[(x, y-1)]
-                node.neighbours.append(north_node)
+            node.neighbours.append(north_node)
+
+            west_node = Plot((x-1, y), None)
+            if x > 0:
+                west_node = plots[(x-1, y)]
+            node.neighbours.append(west_node)
+
+            south_node = Plot((x, y+1), None)
             if y < self.size-1:
                 south_node = plots[(x, y+1)]
-                node.neighbours.append(south_node)
+            node.neighbours.append(south_node)
+
         self.plots = list(plots.values())
 
     def get_regions(self):
@@ -71,16 +76,41 @@ class Garden:
         for region in self.regions:
             region.area = len(region.plots)
 
-    def get_region_perimeters(self):
+    def get_region_perimeters_and_sides(self):
         for region in self.regions:
-            perimeter = 0
+            region.perimeter = 0
+            horizontal_sides = []
+            vertical_sides = []
             for plot in region.plots:
-                plot_perimeter = 4
-                for neighbour in plot.neighbours:
-                    if neighbour.plant == plot.plant:
-                        plot_perimeter -= 1
-                perimeter += plot_perimeter
-            region.perimeter = perimeter
+                for index, neighbour in enumerate(plot.neighbours):
+                    if neighbour.plant != plot.plant:
+                        region.perimeter += 1
+                        if index % 2 != 0:
+                            horizontal_sides.append(neighbour)
+                        else:
+                            vertical_sides.append(neighbour)
+
+            horizontal_sides.sort(key=lambda plot: (plot.y, plot.x))
+            horizontal_sides_count = 0
+            last_x = -float('inf')
+            last_y = -float('inf')
+            for plot in horizontal_sides:
+                if plot.x != last_x+1 or plot.y != last_y:
+                    horizontal_sides_count += 1
+                last_x = plot.x
+                last_y = plot.y
+
+            vertical_sides.sort(key=lambda plot: (plot.x, plot.y))
+            vertical_sides_count = 0
+            last_x = -float('inf')
+            last_y = -float('inf')
+            for plot in vertical_sides:
+                if plot.y != last_y+1 or plot.x != last_x:
+                    vertical_sides_count += 1
+                last_x = plot.x
+                last_y = plot.y
+
+            region.sides = horizontal_sides_count + vertical_sides_count
 
 
 class Region:
@@ -88,12 +118,9 @@ class Region:
     def __init__(self, plant, seed_plot):
         self.plant = plant
         self.plots = [seed_plot]
-        self.area = 1
-        self.perimeter = 4
-        self.setup()
-
-    def setup(self):
-        pass
+        self.area = None
+        self.perimeter = None
+        self.sides = None
 
 
 class Plot:
@@ -105,6 +132,11 @@ class Plot:
         self.region = None
 
 
+with open("12.txt") as _:
+    puzzle_input = _.read().strip()
+
 garden_map = Garden(puzzle_input)
 
 print(sum([region.area*region.perimeter for region in garden_map.regions]))
+
+print(sum([region.area*region.sides for region in garden_map.regions]))
