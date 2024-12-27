f39daf375ef9ac020c96b9c6097c3f8701d3b2a3
Francesco Di Niccolo <francescodiniccolo@gmail.com>
2024-12-27 10:23:47 +0000

    ...

--- 2024/14.py
+++ 2024/14.py
@@ -1,49 +1,50 @@
-from time import sleep
-import os
-
-
 class Environment:
 
     def __init__(self, width, height, robot_info=None):
         self.width = width
         self.height = height
         self.robots = []
-        if robot_info:
-            self.populate_from_robot_info(robot_info)
+        self.populate_from_robot_info(robot_info)
 
     def populate_from_robot_info(self, robot_info):
-        for row in robot_info.split('\n'):
-            position, velocity = row.split()
-            px, py = map(int, position[2:].split(','))
-            vx, vy = map(int, velocity[2:].split(','))
-            self.robots.append(Robot(self, px, py, vx, vy))
+        if robot_info:
+            for row in robot_info.split('\n'):
+                position, velocity = row.split()
+                px, py = map(int, position[2:].split(','))
+                vx, vy = map(int, velocity[2:].split(','))
+                self.robots.append(Robot(self, px, py, vx, vy))
 
-    def simulate(self, *, seconds=100):
+    def simulate(self, *, seconds=100, easter_egg_search=False):
+
+        def search_for_easter_egg():
+            import numpy as np
+            from PIL import Image
+
+            # empty image
+            image = np.zeros((self.height, self.width), dtype=np.uint8)
+
+            # draw
+            for robot in self.robots:
+                if image[robot.py][robot.px] == 0:
+                    image[robot.py][robot.px] = 1
+                else:
+                    # assuming there shouldn't be robots on the same tile
+                    return
+            image *= 255
+            image = Image.fromarray(image, mode='L')
+            image.save(f'14-{second}.png')
+
+            return second
+
         for second in range(seconds+1):
-            
-            os.system('clear')
-            
             if second:
                 for robot in self.robots:
                     robot.move()
-                    
-            map = [['.' for _ in range(self.width)]
-                   for _ in range(self.height)]
-
-            robot_locations = {}
-            for robot in self.robots:
-                location = (robot.px, robot.py)
-                if location not in robot_locations.keys():
-                    robot_locations[location] = 1
-                else:
-                    robot_locations[location] += 1
-
-            for (x, y), robot_count in robot_locations.items():
-                map[y][x] = str(robot_count)
-
-            print('\n'.join(''.join(row) for row in map))
-
-            # sleep(0.5)
+            if easter_egg_search:
+                easter_egg_second = search_for_easter_egg()
+                if easter_egg_second:
+                    return easter_egg_second
+        return self
 
     def get_safety_factor(self):
         quadrants = {}
@@ -58,7 +59,7 @@ class Environment:
         safety_factor = 1
         for value in quadrants.values():
             safety_factor *= value
-        return safety_factor, quadrants
+        return safety_factor
 
 
 class Robot:
@@ -70,12 +71,6 @@ class Robot:
         self.vy = vy
         self.environment = environment
 
-    def __repr__(self):
-        return f"Robot(p={self.px, self.py}, v={self.vx, self.vy})"
-
-    def __str__(self):
-        return f"Robot(p={self.px, self.py}, v={self.vx, self.vy})"
-
     def move(self):
         self.px += self.vx
         self.py += self.vy
@@ -86,9 +81,8 @@ class Robot:
 with open("14.txt") as _:
     puzzle_input = _.read().strip()
 
-if len(puzzle_input.split('\n')) < 20:
-    environment = Environment(11, 7, puzzle_input)
-else:
-    environment = Environment(101, 103, puzzle_input)
-environment.simulate()
-print(environment.get_safety_factor())
+environment = Environment(101, 103, puzzle_input)
+print(environment.simulate().get_safety_factor())
+
+environment = Environment(101, 103, puzzle_input)
+print(environment.simulate(seconds=10000, easter_egg_search=True))
